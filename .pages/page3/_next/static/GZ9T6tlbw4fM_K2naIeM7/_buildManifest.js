2fbe8c109c395883db7374e6e20ea5851a0dd84c
camAtGitHub <camAtGitHub@users.noreply.github.com>
2025-11-10 17:48:14 +1000

    fun(landing-pages): added random pages

---
+++ .pages/page3/_next/static/GZ9T6tlbw4fM_K2naIeM7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-3d9a7915544d198a.js","static/chunks/742-1a0d778dedb6c9d9.js","static/chunks/pages/index-9837723d7bf17056.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
